35bae9f2b3a1ba91a0a795f747196df53b5e44d7
rober <robertohogas@gmail.com>
2025-09-27 09:23:37 -0400

    fixes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { Button } from "@chakra-ui/react"
+import { Box, Button } from "@chakra-ui/react"
+import { Routes, Route } from "react-router-dom"
+import HomePage from "./pages/HomePage"
+import CreatePage from "./pages/CreatePage"
+import Navbar from "./components/Navbar"
 
 function App() {
   return (
-    <Box minh = {"100vh"} display = "flex" alignItems = "center" justifyContent = "center">
-      {/* <Navbar /> */}
+    <Box minH="100vh">
+      <Navbar />
       <Routes>
-        <Route path = "/" element = {<HomePage />} />
-        <Route path = "/create" element = {<CreatePage />} />
+        <Route path="/" element={<HomePage />} />
+        <Route path="/create" element={<CreatePage />} />
       </Routes>
-    </Box>
+    </Box>  
   )
 }
 
-export default App;
+export default App
