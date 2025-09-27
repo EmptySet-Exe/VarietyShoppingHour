8425aad41daadcf0f0516352cf8d01e689196db8
rober <robertohogas@gmail.com>
2025-09-27 12:11:04 -0400

    Add form

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,10 @@
+import React from 'react'
 import { Box, useColorModeValue} from "@chakra-ui/react"
 import { Routes, Route } from "react-router-dom"
 import HomePage from "./pages/HomePage"
 import CreatePage from "./pages/CreatePage"
 import Navbar from "./components/Navbar"
+
 
 function App() {
   const bg = useColorModeValue("gray.100", "gray.900");
