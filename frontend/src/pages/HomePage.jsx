60c3197552a67d32e35137d121c981f4ffbc804b
rober <robertohogas@gmail.com>
2025-09-27 13:28:20 -0400

    Add 'Current Products' text

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,9 +1,33 @@
 import React from 'react'
+import { Container, VStack , Text, SimpleGrid} from '@chakra-ui/react'
+import { Link } from 'react-router-dom'
 
 const HomePage = () => {
   return (
-    <div>HomePage</div>
+    <Container maxW='container.xl' py={12}>
+      <VStack spacing={8}>
+        <Text
+          fontSize={"30"}
+          fontWeight={"bold"}
+          bgGradient={"linear(to-r, cyan.400, blue.500)"}
+          bgClip={"text"}
+          textAlign={"center"}
+        >
+        Current Products 🚀 
+        </Text>
+        
+        <Text fontSize='x1' textAlign={"center"} fontWeight='bold' color='gray.500'>
+          No products found 😢{"  "}
+          <Link to="/create">
+            <Text as="span" color="blue.500" _hover={{ textDecoration: "underline" }}>
+              Create new product
+            </Text>
+          </Link> 
+        </Text>
+      </VStack> 
+      
+    </Container>
   )
 }
 
-export default HomePage
+export default HomePage;
