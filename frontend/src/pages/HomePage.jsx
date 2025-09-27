44114d3637f95c7145b7af112a8d4a1745bb2e07
rober <robertohogas@gmail.com>
2025-09-27 13:47:36 -0400

    Establish the position of each product in the DB

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
 import { Container, VStack , Text, SimpleGrid} from '@chakra-ui/react'
 import { Link } from 'react-router-dom'
+import { useEffect } from 'react'
 
 const HomePage = () => {
+  const { fetchProducts, products}  = UseProductStore(); // from store/product.js
+
+  useEffect(() => {
+    fetchProducts();
+  }, [fetchProducts]);
+
+  console.log("Products:", products);
+    
+
+
   return (
     <Container maxW='container.xl' py={12}>
       <VStack spacing={8}>
@@ -15,6 +26,12 @@ const HomePage = () => {
         >
         Current Products 🚀 
         </Text>
+
+        <SimpleGrid columns={{ base: 1, md: 2, lg: 3 }} spacing={10} w="full">
+          {products.map((product) => (
+            <ProductCard key={product._id} product={product} />
+            ))}
+        </SimpleGrid>
         
         <Text fontSize='x1' textAlign={"center"} fontWeight='bold' color='gray.500'>
           No products found 😢{"  "}
