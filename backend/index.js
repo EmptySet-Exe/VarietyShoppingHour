d34b2c8c2539aa88ecd422247522d2942ec9a996
Jack_Exe <46509186+EmptySet-Exe@users.noreply.github.com>
2025-09-27 10:06:12 -0400

    Initializes the frontend and backend structure
    * Creates and confirmed a running frontend
    * Creates and confirmed a running backend

---
+++ backend/index.js
@@ -0,0 +1,25 @@
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+const port = 3000;  
+
+
+
+app.get("/", (req, res) =>
+{res.send("Sucessfully called the web server")})
+
+const connectDB = async () => 
+    {
+        try {
+            await mongoose.connect(process.env.MONGODB_URI);
+            console.log("Connected to MongoDB")
+            app.listen(port, () => 
+                {console.log("Server is running on port 3000")})
+        }
+        catch(err) {
+            console.log(err);
+            process.exit(1);
+        }
+    }
+    connectDB();
