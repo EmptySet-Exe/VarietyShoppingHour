eee3dbf644311e8192fadb82ba8a652edac47b42
Strigo Araneo <shanmukhaharshith@gmail.com>
2025-09-27 22:55:40 -0400

    Working suggestions from Gemini

--- backend/index.js
+++ backend/index.js
@@ -28,55 +28,8 @@ const connectDB = async () =>
 // ============== NEW CODE FOR DATABASES ===============
 
 const productRoutes = require("./routes/product.route.js");
+const aiRoutes = require("./routes/gemini.route.js");
 
 app.use(express.json());
 app.use("/api/products", productRoutes);
-
-// =============== AI STUFF =============================
-/**
- * @license
- * Copyright 2025 Google LLC
- * SPDX-License-Identifier: Apache-2.0
- */
-const { GoogleGenAI } = require('@google/genai');
-
-const GEMINI_API_KEY = process.env.GEMINI_API_KEY;
-const GOOGLE_CLOUD_PROJECT = process.env.GOOGLE_CLOUD_PROJECT;
-const GOOGLE_CLOUD_LOCATION = process.env.GOOGLE_CLOUD_LOCATION;
-const GOOGLE_GENAI_USE_VERTEXAI = process.env.GOOGLE_GENAI_USE_VERTEXAI;
-
-async function generateContentFromMLDev() {
-  const ai = new GoogleGenAI({vertexai: false, apiKey: GEMINI_API_KEY});
-  const response = await ai.models.generateContent({
-    model: 'gemini-2.0-flash',
-    contents: 'suggest a recipie for me to try',
-  });
-  console.debug("working:", response.text);
-}
-
-// async function generateContentFromVertexAI() {
-//   const ai = new GoogleGenAI({
-//     vertexai: false,
-//     project: GOOGLE_CLOUD_PROJECT,
-//     location: GOOGLE_CLOUD_LOCATION,
-//   });
-//   const response = await ai.models.generateContent({
-//     model: 'gemini-2.0-flash',
-//     contents: 'why is the sky blue?',
-//   });
-//   console.debug(response.text);
-// }
-
-async function main() {
-//   if (GOOGLE_GENAI_USE_VERTEXAI) {
-//     await generateContentFromVertexAI().catch((e) =>
-//       console.error('got error', e),
-//     );
-//   } else {
-    await generateContentFromMLDev().catch((e) =>
-      console.error('got error', e),
-    );
-//   }
-}
-
-main();
+app.use("/aichat", aiRoutes);
