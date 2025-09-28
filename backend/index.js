075b0d192c2acec024e9c5e6e4b0c3d70689bbbb
Strigo Araneo <shanmukhaharshith@gmail.com>
2025-09-27 20:48:38 -0400

    Still unfinished AI stuff

--- backend/index.js
+++ backend/index.js
@@ -49,34 +49,34 @@ async function generateContentFromMLDev() {
   const ai = new GoogleGenAI({vertexai: false, apiKey: GEMINI_API_KEY});
   const response = await ai.models.generateContent({
     model: 'gemini-2.0-flash',
-    contents: 'why is the sky blue?',
+    contents: 'suggest a recipie for me to try',
   });
   console.debug("working:", response.text);
 }
 
-async function generateContentFromVertexAI() {
-  const ai = new GoogleGenAI({
-    vertexai: true,
-    project: GOOGLE_CLOUD_PROJECT,
-    location: GOOGLE_CLOUD_LOCATION,
-  });
-  const response = await ai.models.generateContent({
-    model: 'gemini-2.0-flash',
-    contents: 'why is the sky blue?',
-  });
-  console.debug(response.text);
-}
+// async function generateContentFromVertexAI() {
+//   const ai = new GoogleGenAI({
+//     vertexai: false,
+//     project: GOOGLE_CLOUD_PROJECT,
+//     location: GOOGLE_CLOUD_LOCATION,
+//   });
+//   const response = await ai.models.generateContent({
+//     model: 'gemini-2.0-flash',
+//     contents: 'why is the sky blue?',
+//   });
+//   console.debug(response.text);
+// }
 
 async function main() {
-  if (GOOGLE_GENAI_USE_VERTEXAI) {
-    await generateContentFromVertexAI().catch((e) =>
-      console.error('got error', e),
-    );
-  } else {
+//   if (GOOGLE_GENAI_USE_VERTEXAI) {
+//     await generateContentFromVertexAI().catch((e) =>
+//       console.error('got error', e),
+//     );
+//   } else {
     await generateContentFromMLDev().catch((e) =>
       console.error('got error', e),
     );
-  }
+//   }
 }
 
 main();
